5b82aac517e62213987c81490a89b795beed2ab9
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-12-06 17:08:42 +0330

    Complete getting file input from terminal

--- SAYAC_Assembler.py
+++ SAYAC_Assembler.py
@@ -1,1 +1,11 @@
 import sys
+
+VERSION = "v1.0.0"
+print(f"SAYAC Assembler {VERSION}")
+if len(sys.argv) < 2:
+    print("Error: Not enough arguments --> [file name not found]")
+    exit(1)
+
+insFileName = sys.argv[1]
+print(insFileName)
+
