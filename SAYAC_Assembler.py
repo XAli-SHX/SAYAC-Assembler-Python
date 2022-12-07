3702cac6896363d86171c94fa6540a3732d56ac2
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-12-07 11:15:51 +0330

    Cleanup code
    - Remove all global variables and move them into a function
    - Move the app main run into condition if __name__ == "__main__"

--- SAYAC_Assembler.py
+++ SAYAC_Assembler.py
@@ -1,7 +1,7 @@
 import sys
 
 # constants
-VERSION = "v1.0.0-alpha01"
+VERSION = "v1.0.0-alpha02"
 
 INS_LdR = "ldr"  # load from memory
 INS_LdRio = "ldrio"  # load from I/O peripheral
@@ -70,22 +70,24 @@ INS_REQUIRED_ARGS_COUNT = {
 }
 
 
+def main():
+    # App info
+    print(f"SAYAC Assembler {VERSION}")
+
+    if len(sys.argv) < 2:
+        print("Error: Not enough arguments --> [file name not found]")
+        exit(1)
+
+    # get file name from terminal
+    insFileName = sys.argv[1]
+    binFileLines = []
+    assemble(insFileName, binFileLines)
+
+
 # Exceptions
 class AssemblySyntaxError(Exception):
     def __init__(self, message):
         self.message = message
-
-
-# App info
-print(f"SAYAC Assembler {VERSION}")
-
-if len(sys.argv) < 2:
-    print("Error: Not enough arguments --> [file name not found]")
-    exit(1)
-
-# get file name from terminal
-insFileName = sys.argv[1]
-binFileLines = []
 
 
 def hexToInt(num: str):
@@ -111,160 +113,165 @@ def parseInstruction(ins, line):
     if insType == INS_LdR:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         rs1 = insSplitted[2].replace("_", "").replace("r", "")
-        binFileLines.append(f"0010_00_0_0_{intToBin(rs1, 4)}_{intToBin(rd, 4)}")
+        return f"0010_00_0_0_{intToBin(rs1, 4)}_{intToBin(rd, 4)}"
     elif insType == INS_LdRio:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         rs1 = insSplitted[2].replace("_", "").replace("r", "")
-        binFileLines.append(f"0010_00_1_0_{intToBin(rs1, 4)}_{intToBin(rd, 4)}")
+        return f"0010_00_1_0_{intToBin(rs1, 4)}_{intToBin(rd, 4)}"
     elif insType == INS_STR:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         rs1 = insSplitted[2].replace("_", "").replace("r", "")
-        binFileLines.append(f"0010_01_0_0_{intToBin(rs1, 4)}_{intToBin(rd, 4)}")
+        return f"0010_01_0_0_{intToBin(rs1, 4)}_{intToBin(rd, 4)}"
         pass
     elif insType == INS_STRio:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         rs1 = insSplitted[2].replace("_", "").replace("r", "")
-        binFileLines.append(f"0010_01_1_0_{intToBin(rs1, 4)}_{intToBin(rd, 4)}")
+        return f"0010_01_1_0_{intToBin(rs1, 4)}_{intToBin(rd, 4)}"
         pass
     elif insType == INS_JMR:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         rs1 = insSplitted[2].replace("_", "").replace("r", "")
-        binFileLines.append(f"0010_10_0_0_{intToBin(rs1, 4)}_{intToBin(rd, 4)}")
+        return f"0010_10_0_0_{intToBin(rs1, 4)}_{intToBin(rd, 4)}"
         pass
     elif insType == INS_JMRs:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         rs1 = insSplitted[2].replace("_", "").replace("r", "")
-        binFileLines.append(f"0010_10_1_0_{intToBin(rs1, 4)}_{intToBin(rd, 4)}")
+        return f"0010_10_1_0_{intToBin(rs1, 4)}_{intToBin(rd, 4)}"
     elif insType == INS_JMI:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         imm = insSplitted[2]
-        binFileLines.append(f"0010_11_{intToBin(imm, 6)}_{intToBin(rd, 4)}")
+        return f"0010_11_{intToBin(imm, 6)}_{intToBin(rd, 4)}"
     elif insType == INS_ANR:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         rs1 = insSplitted[2].replace("_", "").replace("r", "")
         rs2 = insSplitted[3].replace("_", "").replace("r", "")
-        binFileLines.append(f"0011_{intToBin(rs1, 4)}_{intToBin(rs2, 4)}_{intToBin(rd, 4)}")
+        return f"0011_{intToBin(rs1, 4)}_{intToBin(rs2, 4)}_{intToBin(rd, 4)}"
     elif insType == INS_ANI:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         imm = insSplitted[2]
-        binFileLines.append(f"0100_{intToBin(imm, 8)}_{intToBin(rd, 4)}")
+        return f"0100_{intToBin(imm, 8)}_{intToBin(rd, 4)}"
     elif insType == INS_MSI:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         imm = insSplitted[2]
-        binFileLines.append(f"0101_{intToBin(imm, 8)}_{intToBin(rd, 4)}")
+        return f"0101_{intToBin(imm, 8)}_{intToBin(rd, 4)}"
     elif insType == INS_MHI:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         imm = insSplitted[2]
-        binFileLines.append(f"0110_{intToBin(imm, 8)}_{intToBin(rd, 4)}")
+        return f"0110_{intToBin(imm, 8)}_{intToBin(rd, 4)}"
     elif insType == INS_SLR:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         rs1 = insSplitted[2].replace("_", "").replace("r", "")
         rs2 = insSplitted[3].replace("_", "").replace("r", "")
-        binFileLines.append(f"0111_{intToBin(rs1, 4)}_{intToBin(rs2, 4)}_{intToBin(rd, 4)}")
+        return f"0111_{intToBin(rs1, 4)}_{intToBin(rs2, 4)}_{intToBin(rd, 4)}"
     elif insType == INS_SAR:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         rs1 = insSplitted[2].replace("_", "").replace("r", "")
         rs2 = insSplitted[3].replace("_", "").replace("r", "")
-        binFileLines.append(f"1000_{intToBin(rs1, 4)}_{intToBin(rs2, 4)}_{intToBin(rd, 4)}")
+        return f"1000_{intToBin(rs1, 4)}_{intToBin(rs2, 4)}_{intToBin(rd, 4)}"
     elif insType == INS_ADD:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         rs1 = insSplitted[2].replace("_", "").replace("r", "")
         rs2 = insSplitted[3].replace("_", "").replace("r", "")
-        binFileLines.append(f"1001_{intToBin(rs1, 4)}_{intToBin(rs2, 4)}_{intToBin(rd, 4)}")
+        return f"1001_{intToBin(rs1, 4)}_{intToBin(rs2, 4)}_{intToBin(rd, 4)}"
     elif insType == INS_ADR:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         rs1 = insSplitted[2].replace("_", "").replace("r", "")
         rs2 = insSplitted[3].replace("_", "").replace("r", "")
-        binFileLines.append(f"1001_{intToBin(rs1, 4)}_{intToBin(rs2, 4)}_{intToBin(rd, 4)}")
+        return f"1001_{intToBin(rs1, 4)}_{intToBin(rs2, 4)}_{intToBin(rd, 4)}"
     elif insType == INS_SUB:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         rs1 = insSplitted[2].replace("_", "").replace("r", "")
         rs2 = insSplitted[3].replace("_", "").replace("r", "")
-        binFileLines.append(f"1010_{intToBin(rs1, 4)}_{intToBin(rs2, 4)}_{intToBin(rd, 4)}")
+        return f"1010_{intToBin(rs1, 4)}_{intToBin(rs2, 4)}_{intToBin(rd, 4)}"
     elif insType == INS_SUR:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         rs1 = insSplitted[2].replace("_", "").replace("r", "")
         rs2 = insSplitted[3].replace("_", "").replace("r", "")
-        binFileLines.append(f"1010_{intToBin(rs1, 4)}_{intToBin(rs2, 4)}_{intToBin(rd, 4)}")
+        return f"1010_{intToBin(rs1, 4)}_{intToBin(rs2, 4)}_{intToBin(rd, 4)}"
     elif insType == INS_ADI:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         imm = insSplitted[2]
-        binFileLines.append(f"1011_{intToBin(imm, 8)}_{intToBin(rd, 4)}")
+        return f"1011_{intToBin(imm, 8)}_{intToBin(rd, 4)}"
     elif insType == INS_SUI:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         imm = insSplitted[2]
-        binFileLines.append(f"1100_{intToBin(imm, 8)}_{intToBin(rd, 4)}")
+        return f"1100_{intToBin(imm, 8)}_{intToBin(rd, 4)}"
     elif insType == INS_MUL:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         rs1 = insSplitted[2].replace("_", "").replace("r", "")
         rs2 = insSplitted[3].replace("_", "").replace("r", "")
-        binFileLines.append(f"1101_{intToBin(rs1, 4)}_{intToBin(rs2, 4)}_{intToBin(rd, 4)}")
+        return f"1101_{intToBin(rs1, 4)}_{intToBin(rs2, 4)}_{intToBin(rd, 4)}"
     elif insType == INS_DIV:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         rs1 = insSplitted[2].replace("_", "").replace("r", "")
         rs2 = insSplitted[3].replace("_", "").replace("r", "")
-        binFileLines.append(f"1110_{intToBin(rs1, 4)}_{intToBin(rs2, 4)}_{intToBin(rd, 4)}")
+        return f"1110_{intToBin(rs1, 4)}_{intToBin(rs2, 4)}_{intToBin(rd, 4)}"
     elif insType == INS_CMR:
         rs1 = insSplitted[1].replace("_", "").replace("r", "")
         rs2 = insSplitted[2].replace("_", "").replace("r", "")
-        binFileLines.append(f"1111_000_0_{intToBin(rs2, 4)}_{intToBin(rs1, 4)}")
+        return f"1111_000_0_{intToBin(rs2, 4)}_{intToBin(rs1, 4)}"
     elif insType == INS_CMI:
         imm = insSplitted[1]
         rs1 = insSplitted[2].replace("_", "").replace("r", "")
-        binFileLines.append(f"1111_001_{intToBin(imm, 5)}_{intToBin(rs1, 4)}")
+        return f"1111_001_{intToBin(imm, 5)}_{intToBin(rs1, 4)}"
     elif insType == INS_BRC:
         FIB = insSplitted[1]
         rd = insSplitted[2].replace("_", "").replace("r", "")
-        binFileLines.append(f"1111_010_{intToBin(str(hexToInt(FIB)), 5)}_{intToBin(rd, 4)}")
+        return f"1111_010_{intToBin(str(hexToInt(FIB)), 5)}_{intToBin(rd, 4)}"
     elif insType == INS_BRR:
         FIB = insSplitted[1]
         rd = insSplitted[2].replace("_", "").replace("r", "")
-        binFileLines.append(f"1111_011_{intToBin(str(hexToInt(FIB)), 5)}_{intToBin(rd, 4)}")
+        return f"1111_011_{intToBin(str(hexToInt(FIB)), 5)}_{intToBin(rd, 4)}"
     elif insType == INS_SHI:
         shimm = insSplitted[1]
         rd = insSplitted[2].replace("_", "").replace("r", "")
-        binFileLines.append(f"1111_10_0_{intToBin(shimm, 5)}_{intToBin(rd, 4)}")
+        return f"1111_10_0_{intToBin(shimm, 5)}_{intToBin(rd, 4)}"
     elif insType == INS_SHIla:
         shimm = insSplitted[1]
         rd = insSplitted[2].replace("_", "").replace("r", "")
-        binFileLines.append(f"1111_10_1_{intToBin(shimm, 5)}_{intToBin(rd, 4)}")
+        return f"1111_10_1_{intToBin(shimm, 5)}_{intToBin(rd, 4)}"
     elif insType == INS_NTR:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         rs1 = insSplitted[2].replace("_", "").replace("r", "")
-        binFileLines.append(f"1111_110_0_{intToBin(rs1, 4)}_{intToBin(rd, 4)}")
+        return f"1111_110_0_{intToBin(rs1, 4)}_{intToBin(rd, 4)}"
     elif insType == INS_NTR2c:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         rs1 = insSplitted[2].replace("_", "").replace("r", "")
-        binFileLines.append(f"1111_110_1_{intToBin(rs1, 4)}_{intToBin(rd, 4)}")
+        return f"1111_110_1_{intToBin(rs1, 4)}_{intToBin(rd, 4)}"
     elif insType == INS_NTD:
         rd = insSplitted[1].replace("_", "").replace("r", "")
-        binFileLines.append(f"1111_111_0_0000_{intToBin(rd, 4)}")
+        return f"1111_111_0_0000_{intToBin(rd, 4)}"
     elif insType == INS_NTD2c:
         rd = insSplitted[1].replace("_", "").replace("r", "")
-        binFileLines.append(f"1111_111_1_0000_{intToBin(rd, 4)}")
+        return f"1111_111_1_0000_{intToBin(rd, 4)}"
     else:
         raise Exception("Uncaught!")
 
 
-try:
-    # open the SAYAC Assembly code from the path given
-    insFile = open(insFileName, "r")
-    insLines = insFile.readlines()
-    insFile.close()
-    for lineIndex in range(0, len(insLines)):
-        parseInstruction(insLines[lineIndex], lineIndex + 1)
-    binFileName = insFileName.rsplit(".", maxsplit=1)[0]
-    binFile = open(binFileName + ".bin", "w")
-    binFile.write("\n")
-    for lineIndex in range(0, len(insLines)):
-        binFile.write(f"{binFileLines[lineIndex]}\n")
-    binFile.close()
-    print("Successfully Assembled!")
-except FileNotFoundError:
-    print(f"Error: File not found --> ['{insFileName}' does not exists]")
-except KeyError as e:
-    print(f"Error: Instruction {e} not supported yet")
-except AssemblySyntaxError as e:
-    print(f"Error: {e.message}")
-except Exception as e:
-    print(f"Error: Unhandled exception --> [{e}]")
+def assemble(insFileName, binFileLines):
+    try:
+        # open the SAYAC Assembly code from the path given
+        insFile = open(insFileName, "r")
+        insLines = insFile.readlines()
+        insFile.close()
+        for lineIndex in range(0, len(insLines)):
+            binFileLines.append(parseInstruction(insLines[lineIndex], lineIndex + 1))
+        binFileName = insFileName.rsplit(".", maxsplit=1)[0]
+        binFile = open(binFileName + ".bin", "w")
+        binFile.write("\n")
+        for lineIndex in range(0, len(insLines)):
+            binFile.write(f"{binFileLines[lineIndex]}\n")
+        binFile.close()
+        print("Successfully Assembled!")
+    except FileNotFoundError:
+        print(f"Error: File not found --> ['{insFileName}' does not exists]")
+    except KeyError as e:
+        print(f"Error: Instruction {e} not supported yet")
+    except AssemblySyntaxError as e:
+        print(f"Error: {e.message}")
+    except Exception as e:
+        print(f"Error: Unhandled exception --> [{e}]")
+
+
+if __name__ == "__main__":
+    main()
