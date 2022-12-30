fb16ab2cdaae842eddc8be32b3ea346a378af5b4
Ali Shayanpoor <shayanpoorali66@gmail.com>
2022-12-30 20:03:28 +0330

    Add ANR instruction

--- SAYAC_Assembler.py
+++ SAYAC_Assembler.py
@@ -287,10 +287,11 @@ def parseInstruction(ins, line, sayac: Sayac):
         sayac.registers[int(rd)] = sayac.PC + 1
         sayac.PC += int(imm)
     elif insType == INS_ANR:
+        # ANR rd rs1 rs2
         rd = insSplitted[1].replace("_", "").replace("r", "")
         rs1 = insSplitted[2].replace("_", "").replace("r", "")
         rs2 = insSplitted[3].replace("_", "").replace("r", "")
-        return f"0011_{intToBin(rs1, 4)}_{intToBin(rs2, 4)}_{intToBin(rd, 4)}"
+        sayac.registers[int(rd)] = sayac.registers[int(rs1)] & sayac.registers[int(rs2)]
     elif insType == INS_AND:
         rd = insSplitted[1].replace("_", "").replace("r", "")
         rs1 = insSplitted[2].replace("_", "").replace("r", "")
